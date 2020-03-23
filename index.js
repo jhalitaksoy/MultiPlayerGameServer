f003340a628650220f25ffd3819f0d260d7b4582
Halit Aksoy <jhalitaksoy@gmail.com>
2020-03-23 15:39:29 +0300

    Stop serving static files

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
